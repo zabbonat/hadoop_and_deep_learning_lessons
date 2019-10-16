0747e5954d9b0a6907ed8589e5eebccecfffbafd
Vamsi Krishna Varma <vamsi.gunturi@gmail.com>
2019-10-16 14:28:32 +0200

    Some more examples of Mapreduce
    Word Frequency
    Customer Orders

---
+++ Python_Map_Reduce/customers_orders/customer_orders.py
@@ -0,0 +1,39 @@
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+import re
+
+class MRCustomerOrderCounter(MRJob):
+    def mapper(self, key, line):
+        (custID, itemID, orderAmt) = line.split(',')
+        yield custID, float(orderAmt)
+
+    def reducer(self, custID, orders):
+        yield custID, sum(orders)
+
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper_get_customers,
+                   reducer=self.reducer_count_orders),
+            MRStep(mapper=self.mapper_make_orders_key,
+                   reducer = self.reducer_output_orders)
+        ]
+
+    def mapper_get_customers(self, key, line):
+        (custID, itemID, orderAmt) = line.split(',')
+        yield custID, float(orderAmt)
+
+    def reducer_count_orders(self, custID, orders):
+        yield custID, sum(orders)
+
+    def mapper_make_orders_key(self, custID, count):
+        yield '%04d'%int(count), custID
+
+    def reducer_output_orders(self, count, customers):
+        for custID in customers:
+            yield count, custID
+
+if __name__ == '__main__':
+    MRCustomerOrderCounter.run()
+
+
+
